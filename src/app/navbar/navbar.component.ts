fee86e2716d6daf9e8514d13e7eceda18a199bba
nontawit <ntwitmn@gmail.com>
2024-02-20 00:52:54 +0700

    edit navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  showMenu = false;
 
+  toggleMenu(){
+    this.showMenu = !this.showMenu;
+  }
 }
