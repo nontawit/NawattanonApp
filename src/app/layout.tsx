a7981745f0947a77a72874cedf6b000e64b90300
nontawit <ntwitmn@gmail.com>
2024-02-28 01:59:19 +0700

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/nav";
+import Navbar from "../components/nav.tsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
