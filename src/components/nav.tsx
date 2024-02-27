2caabbe47709130e7f939f39c4ee4e235de29d3d
nontawit <ntwitmn@gmail.com>
2024-02-28 02:10:02 +0700

    Update nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -26,8 +26,8 @@ function Nav() {
                                     stroke=" currentColor "
                                 >
                                     <path
-                                        strokeLinecap=" round "
-                                        strokeLinejoin=" round "
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         strokeWidth="2"
                                         d="M6 18L18 6M6 6l12 12"
                                     />
@@ -41,8 +41,8 @@ function Nav() {
                                     stroke=" currentColor "
                                 >
                                     <path
-                                        strokeLinecap=" round "
-                                        strokeLinejoin=" round "
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
                                         strokeWidth="2"
                                         d="M4 6h16M4 12h16M4 18h16"
                                     />
