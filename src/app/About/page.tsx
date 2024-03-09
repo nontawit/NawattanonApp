d7b60f39be33c8983d28d7f086d3631a61e2d994
nontawit <ntwitmn@gmail.com>
2024-03-10 02:06:21 +0700

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,14 +3,19 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <ul>
-        <li className="text-lg">งานระบบไฟฟ้าแรงสูง - แรงต่ำ, จำหน่ายอุปกรณ์</li>
-        <li className="text-lg">งานบำรุงรักษาหม้อแปลงไฟฟ้า และตู้ MDB</li>
-        <li className="text-lg">งานติดตั้งหม้อแปลงไฟฟ้า, ปักเสาไฟฟ้า, อุปกรณ์ประกอบ</li>
-        <li className="text-lg">งานส่องจุดความร้อน Thermoscan</li>
-        <li className="text-lg">งานแก้ไขระบบกราวด์แรงสูง, ระบบกราวด์แรงต่ำ</li>
-      </ul>
+    <div className=" mt-10">
+      <p className="text-2xl font-bold text-center">
+        เกี่ยวกับเรา
+      </p>
+      <div>
+        <ul className="mt-5">
+          <li className="text-lg">งานระบบไฟฟ้าแรงสูง - แรงต่ำ, จำหน่ายอุปกรณ์</li>
+          <li className="text-lg">งานบำรุงรักษาหม้อแปลงไฟฟ้า และตู้ MDB</li>
+          <li className="text-lg">งานติดตั้งหม้อแปลงไฟฟ้า, ปักเสาไฟฟ้า, อุปกรณ์ประกอบ</li>
+          <li className="text-lg">งานส่องจุดความร้อน Thermoscan</li>
+          <li className="text-lg">งานแก้ไขระบบกราวด์แรงสูง, ระบบกราวด์แรงต่ำ</li>
+        </ul>
+      </div>
     </div>
   )
 }
