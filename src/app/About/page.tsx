cc098eaf595a8e8a61593150ca32a1dc707c816c
nontawit <ntwitmn@gmail.com>
2024-03-12 00:32:07 +0700

    test

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className=" mt-5">
+    <div className=" mt-10">
       <p className="text-2xl font-bold text-center">
         เกี่ยวกับเรา
       </p>
