d19b64530c7a2f98c20b317200a6d3cc6f4e1a1a
nontawit <ntwitmn@gmail.com>
2024-03-12 00:59:26 +0700

    Update page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className=" mt-2">
+    <div>
       <p className="text-2xl font-bold text-center">
         ติดต่อเรา
       </p>
