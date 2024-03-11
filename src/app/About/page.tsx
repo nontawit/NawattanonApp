d7fa592e78e341288b5905f8b315e3dcb15dad88
nontawit <ntwitmn@gmail.com>
2024-03-12 01:05:03 +0700

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className=" mt-2">
+    <div>
       <p className="text-2xl font-bold text-center">
         เกี่ยวกับเรา
       </p>
